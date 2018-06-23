e85a5dedfb9b52aaf454e750c30de97df90bcce5
Umyippa <sywhitehouse@gmail.com>
2018-06-23 17:35:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ function nowServing(line) {
 
 function currentLine(line) {
   if(!line.length) {
-    return "The lined is currently empty."
+    return "The line is currently empty."
   }
   const nAndN = []
   
   for (let i = 0, l = line.length; i < l; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`)
+    nAndN.push(`${i + 1}. ${line[i]}`)
   }
   return `The line is currently: ${numbersAndNames.join(', ')}`
 }
