3754889f9bb8599f98bfdf0fb0d4feab8f4eeda6
Umyippa <sywhitehouse@gmail.com>
2018-06-23 17:29:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,5 +11,7 @@ function nowServing(line) {
     return "There is nobody waiting to be served!"
   }
   
-  return "Currently serving ${line.shift}"
+  return `Currently serving ${line.shift().`{
+    
+  }
 }
