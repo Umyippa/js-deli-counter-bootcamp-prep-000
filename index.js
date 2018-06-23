18070c1874695de1c8098e309979fa84ed261423
Umyippa <sywhitehouse@gmail.com>
2018-06-23 17:03:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var otherDeli = [];
 
 function takeANumber(line, name) {
   line.push(name)
-  return `Welcome ${name}. You are number ${line.length} in line.`
+  return `Welcome, ${name}. You are number ${line.length} in line.`
 }
