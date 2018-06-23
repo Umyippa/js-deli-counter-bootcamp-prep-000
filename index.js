63539385451a96608818763bbd9b548d61893dc9
Umyippa <sywhitehouse@gmail.com>
2018-06-23 16:54:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var otherDeli = [];
 
 function takeANumber(katzDeliLine, otherDeli) {
   takeANumber = (katzDeliLine, otherDeli);
-  return takeANumber;
+  return (`Welcome, `[otherDeli]`. You are number `[katzDeliLine] ` in line`)
 }
