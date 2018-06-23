d23b4a7c17a6157bfb8d9ec4081d0787031fbd90
Umyippa <sywhitehouse@gmail.com>
2018-06-23 17:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,5 @@ function currentLine(line) {
   for (let i = 0, l = line.length; i < l; i++) {
     numbersAndNames.push(`${i + 1}. ${line[i]}`)
   }
-  return 
+  return `The line is currently: ${numbersAndNames.join(', ')}`
 }
